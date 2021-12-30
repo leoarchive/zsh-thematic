db286f711b2cc0dff16c41e4c8b248c4581dc95d
leozamboni <leonardonunes169@gmail.com>
2021-12-30 09:36:01 -0300

    :sparkles: feat: thematic theme

--- zsh-thematic.zsh
+++ zsh-thematic.zsh
@@ -1,5 +1,20 @@
 #!/bin/bash
+# Zsh thematic 
+
 # Copyright (c) 2021 Leonardo Z. Nunes
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 MONTH=$(date +%b)
 DAY=$(date +%d)
@@ -11,6 +26,9 @@ case $MONTH in
 	"Jun") # PRIDE MONTH
 		EMOJI="🌈"
 		;;
+	"Oct") # HALLOWEEN
+		EMOJI="🎃"
+		;; 
 	"Dec") # MARRY CHRISTMAS
 		EMOJI="🎄"
 		if [[ DAY > 25 ]] # HAPPY NEW YEAR
